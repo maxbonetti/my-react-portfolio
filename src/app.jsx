1ca2ed4eb5034676aea07a7672ed4dbfa2c2d239
Max Bonetti <maxwell.bonetti@gmail.com>
2024-06-05 19:20:52 -0500

    save files and updated folder structure and file pathing

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import './app.css';
+import AboutMe from './pages/AboutMe';
+
+function App() {
+    return (
+        <div className="portfolio-app">
+            <AboutMe />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,4 @@
+import { useState } from 'react';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+import Resume from './Resume';
